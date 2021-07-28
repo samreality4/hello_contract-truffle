02210178455984c77d70a750b093123069da6c16
sam <samaudacity2@gmail.com>
2021-07-27 21:57:40 -0700

    celo contract deployment

--- celo_deploy.js
+++ celo_deploy.js
@@ -5,6 +5,8 @@ const kit = ContractKit.newKitFromWeb3(web3)
 const getAccount = require('./getAccount').getAccount
 // const HelloWorld = require('./build/contracts/HelloWorld.json')
 
+
+
 async function awaitWrapper(){
     let account = await getAccount()
     
@@ -12,7 +14,7 @@ async function awaitWrapper(){
     // get some testnet funds at https://celo.org/build/faucet
     console.log(account.address)
     
-//     kit.connection.addAccount(account.privateKey) // this account must have a CELO balance to pay transaction fees
+    kit.connection.addAccount(account.privateKey) // this account must have a CELO balance to pay transaction fees
 
 //     let tx = await kit.connection.sendTransaction({
 //         from: account.address,
--- truffle-config.js
+++ truffle-config.js
@@ -9,10 +9,11 @@
  * truffleframework.com/docs/advanced/configuration
  */
 
-const Kit = require('@celo/contractkit')
-const kit = Kit.newKit('https://alfajores-forno.celo-testnet.org')
-
-const getAccount = require('./getAccount').getAccount
+ const Web3 = require('web3')
+ const ContractKit = require('@celo/contractkit')
+ const web3 = new Web3('https://alfajores-forno.celo-testnet.org')
+ const kit = ContractKit.newKitFromWeb3(web3)
+ const getAccount = require('./getAccount').getAccount
 
 async function awaitWrapper(){
     let account = await getAccount()
